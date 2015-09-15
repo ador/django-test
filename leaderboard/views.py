8c47799fc38a70b8d155b1bcdc3a2313e24c1bbc
Adrienn Szabo <adrienn.szabo@sztaki.mta.hu>
2015-09-15 11:47:19 +0200

    view added

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Submission, BubiParticipant
 
 # Create your views here.
+
+def submission_list(request):
+    entries = Submission.objects.filter(submission_date__lte=timezone.now()).order_by('submission_date')
+    return render(request, 'leaderboard/leaderboard.html', {'submissions': entries})
